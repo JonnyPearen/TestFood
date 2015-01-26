996f894207287da28c26f5896cd727cbc3e1e64a
denis <dturitsa@gmail.com>
2015-01-26 00:16:36 -0800

    Added  viewlist for the chosen ingredients on the recipe details page,  populated from an array list.

--- app/src/main/java/com/mycompany/testfood/RecipeDetails.java
+++ app/src/main/java/com/mycompany/testfood/RecipeDetails.java
@@ -6,16 +6,23 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RecipeDetails extends ActionBarActivity {
-
+    //arraylist that stores chosen ingredients
+    private List<String> ingredients_array_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
 
         //declare intent
         Intent activityThatCalled = getIntent();
@@ -31,8 +38,28 @@ public class RecipeDetails extends ActionBarActivity {
 
         //append selected string message to the textview on details page.
         searchButtonSelectedMessage.append(" " + searchButtonSelected);
-    }
 
+        ingredients_array_list.add("rice");
+        ingredients_array_list.add("cheese");
+        ingredients_array_list.add("tomatoes");
+        populateIngredientsList(ingredients_array_list);
+
+    }
+    //fills the ingredients listview with with strings from the passed in array list
+    private void populateIngredientsList(List<String> a){
+
+       ListView lv = (ListView) findViewById(R.id.ingredientsListView);
+
+        // This is the array adapter, takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and the
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                a );
+
+        lv.setAdapter(arrayAdapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
